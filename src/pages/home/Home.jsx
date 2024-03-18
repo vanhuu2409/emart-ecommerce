cc388b96487a926e21a2788fc77f519112b7cf3e
vanhuu2409 <vanhuu0924@gmail.com>
2024-03-18 16:25:52 +0700

    react lazy

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,27 +1,35 @@
-import Arrivals from "./components/Arrivals/Arrivals";
-import Induction from "./components/Induction/Induction";
-import VideoCarousel from "./components/VideoCarousel/VideoCarousel";
-import Collections from "./components/Collections/Collections";
-import Trending from "./components/Trending/Trending";
+import { Suspense, lazy } from "react";
+import Loading from "./Loading";
+
+const Arrivals = lazy(() => import("./components/Arrivals/Arrivals"));
+const Induction = lazy(() => import("./components/Induction/Induction"));
+const VideoCarousel = lazy(() =>
+  import("./components/VideoCarousel/VideoCarousel")
+);
+const Collections = lazy(() => import("./components/Collections/Collections"));
+const Trending = lazy(() => import("./components/Trending/Trending"));
+
 const Home = () => {
   return (
-    <div className='mx-auto'>
-      <div className='mx-auto '>
-        <VideoCarousel />
+    <Suspense fallback={<Loading />}>
+      <div className='mx-auto'>
+        <div className='mx-auto '>
+          <VideoCarousel />
+        </div>
+        <div className='mx-auto py-4'>
+          <Arrivals />
+        </div>
+        <div className='mx-auto py-4'>
+          <Induction />
+        </div>
+        <div className='mx-auto py-4 px-4 sm:px-6 md:px-10 lg:px-16'>
+          <Collections />
+        </div>
+        <div className='mx-auto py-4 px-4  sm:px-6 md:px-10 lg:px-16'>
+          <Trending />
+        </div>
       </div>
-      <div className='mx-auto py-4'>
-        <Arrivals />
-      </div>
-      <div className='mx-auto py-4'>
-        <Induction />
-      </div>
-      <div className='mx-auto py-4 px-4 sm:px-6 md:px-10 lg:px-16'>
-        <Collections />
-      </div>
-      <div className='mx-auto py-4 px-4  sm:px-6 md:px-10 lg:px-16'>
-        <Trending />
-      </div>
-    </div>
+    </Suspense>
   );
 };
 
---
+++ src/pages/home/Loading.jsx
@@ -0,0 +1,5 @@
+const Loading = () => {
+  return <div className='text-3xl text-center text-primary'>Loading...</div>;
+};
+
+export default Loading;
